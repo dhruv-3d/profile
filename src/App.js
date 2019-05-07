544ec646d828b3d6caf37d2d6e05897b76250caf
Dhruv Desai <dhruvdesai3d@gmail.com>
2019-05-08 00:58:37 +0530

    starting up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import dhruv from './dhruv.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="intro">
+          <img src={dhruv} className="App-logo" alt="logo" />
+          <p className="whoami">
+            <span className="my-name">Dhruv Desai</span>
+            {/* <span className="status">Python & ReactJS Developer</span> */}
+            <span className="status">Surat, Gujarat, India</span>
+          </p>
+        </div>
       </header>
     </div>
   );

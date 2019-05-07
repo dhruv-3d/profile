5cfb2219e07b285efdbfec935e34b37c15a69e72
Dhruv Desai <dhruvdesai3d@gmail.com>
2019-05-07 23:20:14 +0530

    Updates

---
+++ precache-manifest.7b9acbf337642a578d12d7ed78952d50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1502b0c8dfe27f6080b71d34f315d5a2",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "c68d141812ca0a2745e4",
+    "url": "/profile/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/profile/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c68d141812ca0a2745e4",
+    "url": "/profile/static/js/main.e73ba2db.chunk.js"
+  },
+  {
+    "revision": "189fd871db148202566e",
+    "url": "/profile/static/js/runtime~main.cc8686f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/profile/static/media/logo.5d5d9eef.svg"
+  }
+]);
